0d8a6abe6cf9bd05e20e08790cf262468d9615c2
yuma-88 <ahiru1904@gmail.com>
2026-01-04 17:38:11 +0900

    計算コンソール

--- 01_calculator/Program.cs
+++ 01_calculator/Program.cs
@@ -1,2 +1,98 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO.Pipelines;
+
+class Program
+{
+  static void Main()
+  {
+    try
+    {
+      double dblresult; // 計算結果
+      double dblFirstNum; // 一つ目の値
+      double dblSecondNum; // 二つ目の値
+
+      // タイトルの表示
+      Console.WriteLine("=== 四則計算コンソールアプリ ===");
+
+      // 一つ目の値の処理
+      while (true)
+      {
+        Console.Write("１つ目の数字：");
+        string? strFirstInput = Console.ReadLine();
+
+        if (double.TryParse(strFirstInput, out dblFirstNum))
+        {
+          break;
+        }
+        Console.WriteLine("数値を入力してください。");
+      }
+
+      // 二つ目の値の処理
+      while (true)
+      {
+        Console.Write("２つ目の数字：");
+        string? strSecondInput = Console.ReadLine();
+
+        if (double.TryParse(strSecondInput, out dblSecondNum))
+        {
+          break;
+        }
+        Console.WriteLine("数値を入力してください。");
+      }
+
+      // 演算子の選択
+      string? strOperator;
+      while (true)
+      {
+          Console.Write("演算子を入力（+ - * /）：");
+          strOperator = Console.ReadLine();
+
+          if (strOperator is "+" or "-" or "*" or "/")
+          {
+              break;
+          }
+
+          Console.WriteLine("正しい演算子を入力してください。");
+      }
+
+      // 演算子選択結果による処理
+      switch (strOperator)
+      {
+        case "+": // 「+」の場合
+        {
+          dblresult = dblFirstNum + dblSecondNum;
+          break;
+        }
+        case "-": // 「-」の場合
+        {
+          dblresult = dblFirstNum - dblSecondNum;
+          break;
+        }
+        case "*": // 「*」の場合
+        {
+          dblresult = dblFirstNum * dblSecondNum;
+          break;
+        }
+        case "/": // 「/」の場合
+        {
+          if (dblSecondNum == 0)
+          {
+            Console.WriteLine("0で割ることはできません。");
+            return;
+          }
+          dblresult = dblFirstNum / dblSecondNum;
+          break;
+        }
+        default:
+        {
+          return;
+        }
+      }
+      Console.WriteLine($"結果: {dblresult}");
+    }
+    catch(Exception ex)
+    {
+      Console.WriteLine($"予期しないエラーが発生しました: {ex.Message}");
+    }
+  }
+}
