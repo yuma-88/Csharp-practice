ecebcc47c1962180598ac64550435e7e04f4b7cf
yuma-88 <ahiru1904@gmail.com>
2026-04-12 17:18:35 +0900

    practice/06_timer

---
+++ 06_timer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading;
+class Program
+{
+  static void Main()
+  {
+    string? strInput = "";
+    int intSeconds;
+
+    try
+    {
+      while (true)
+      {
+        Console.Write("秒数(整数)を入力してください：");
+        strInput = Console.ReadLine();
+
+        if(int.TryParse(strInput, out intSeconds))
+        {
+          break;
+        }
+      }
+
+      for (int intCount = intSeconds; intCount > 0; intCount--)
+      {
+        Console.WriteLine("カウントダウン：" + intCount);
+        Thread.Sleep(1000);
+      }
+
+      Console.WriteLine("0");
+      Console.WriteLine("終了");
+    }
+    catch (Exception ex)
+    {
+      Console.WriteLine($"予期しないエラーが発生しました: {ex.Message}");
+    }
+  }
+}
